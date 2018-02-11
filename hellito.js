58a24f43936cc9cdbac79e9b3e7e58999419706e
treejosh.net <edit@treejosh.net>
2018-02-11 14:06:49 +0100

    create hellito.js
    JS script to modify answers

---
+++ hellito.js
@@ -0,0 +1,10 @@
+function chargeTO(){document.write("hellito:---bjr");
+var helliTO3jo=""; 
+function helliRESPto(respOND){
+   document.write("hellito:NUTS supreme");
+   var stockTAB=new array("je bois du cafe","je bois du the","hellito:","3jos");
+   var conSTRRESP=stockTAB[respOND];
+}
+function stockTAB(helliTO3jo){
+helliRESPto(helliTO3jo);
+}
